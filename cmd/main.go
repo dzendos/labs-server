bced183d9a5ff7dd5839657d44e753b4799eb51f
Evgeny <e.gerasimov@mail.com>
2024-09-12 22:58:10 +0300

    img2

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 )
-
-func ImageHandler(w http.ResponseWriter, r *http.Request) {
-	img, err := os.Open("." + r.URL.Path)
-	if err != nil {
-		w.Write([]byte("unknown file: " + "." + r.URL.Path))
-	}
-	defer img.Close()
-	w.Header().Set("Content-Type", "image/jpeg")
-	io.Copy(w, img)
-}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/images/", ImageHandler)
-
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
