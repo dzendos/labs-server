322919158063776b5c1712c5fd9235006d21de86
Evgeny <e.gerasimov@mail.com>
2024-09-12 22:06:47 +0300

    img1

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
