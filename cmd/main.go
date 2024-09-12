0a63531a2d30074a95f98dae04921c1b2cf49196
Evgeny <e.gerasimov@mail.com>
2024-09-13 00:01:21 +0300

    i

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
+
+func ImageHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	imgParts := strings.Split(parts[len(parts)-1], ".")
+	if len(imgParts) != 2 {
+		w.Write([]byte("incorrect file format"))
+	}
+
+	ext := imgParts[1]
+	w.Header().Set("Content-Type", "image/"+ext)
+
+	b, err := os.ReadFile(".images/" +parts[len(parts)-1])
+	if err != nil {
+		w.Write([]byte("unknown file"))
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		w.Write([]byte("unexpected error"))
+	}
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.HandleFunc("/images/", ImageHandler)
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
